a194df19edaa5a60b34d342f29e8c4cd1b890582
armando <armando.lezama.psic@gmail.com>
2021-10-21 18:00:59 -0500

    feat(js)/Added login initial prototype

--- service/public/functions/login.js
+++ service/public/functions/login.js
@@ -1,10 +1,51 @@
-const login = dataBase => (request, response) => {
-  const { userName, userPassword } = request.body;
+/**
+ * TO-DO: Create login functionaliti
+ */
 
+const login = dataBase => async (request, response) => {
+  const { user, password } = request.body;
+  dataBase.collection = 'usersCredentials';
+  dataBase.filter = {user : user};
+  const responseData = {
+    status: 0,
+    payload: {},
+    error: {},
+  };
+
+  const loginSucces = {
+    message: 'welcome'
+  };
+
+  const loginError = {
+    message: 'wrong credentials'
+  };
   
-  response.redirect('/login');
-
+  try {
+    responseData.status = 200;
+    const credential = await dataBase.getDocument();
+    responseData.payload = credential.password === password ? loginSucces : loginError;
+  } catch (error) {
+    responseData.status = 500;
+    responseData.error = error;
+  }
+  response.status(responseData.status);
+  response.send({
+    payload: responseData.payload,
+    error: responseData.error
+  });
 
 };
 
 module.exports = login;
+
+/**
+ * TO-DO: Add token management functionality (Json Web Token)
+ */
+
+/**
+ * TO-DO: Add security good practices, https, tls, etc
+ */
+
+/**
+ * TO-DO: Add different auth methods
+ */
