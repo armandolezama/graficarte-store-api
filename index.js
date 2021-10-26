3ed5c30ec37b2791c844a60bc180717ec9b12c42
armando <armando.lezama.psic@gmail.com>
2021-10-25 20:54:41 -0500

    feat*): Helmet added

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const dotenv = require('dotenv').config();
 const service = require('./Service');
 const { DataBase } = require('./Database');
 const DB = new DataBase();
+const helmet = require("helmet");
 
+app.use(helmet());
 app.use('/', service(DB));
 
 app.listen(process.env.PORT, () => {
     console.log(`Iniciando servidor en ${process.env.PORT}`);
-    console.log(`http://localhost:${process.env.PORT}`)
+    console.log(`http://localhost:${process.env.PORT}`);
+    console.log('Bieeeeen!! :)');
 });
 
 DB.connect().catch(error => {
