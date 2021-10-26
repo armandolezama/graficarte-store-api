97ab1a66c9880b16ea92cfbb356a3e516e8b0107
armando <armando.lezama.psic@gmail.com>
2021-10-25 20:17:11 -0500

    fix(js)/Unused services deleted

--- dataBase/index.js
+++ dataBase/index.js
@@ -1,10 +1,5 @@
-const express = require('express');
-const dataBaseService = express.Router();
 const DataBase = require('./DataBase.js');
 
-
-
 module.exports = {
-  dataBaseService,
   DataBase
 };
